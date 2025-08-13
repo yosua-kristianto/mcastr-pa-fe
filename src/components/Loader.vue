c0a9737222cc824f78a8b10ac2a639735dbbdaf1
Yosua Kristianto (Legion) <yosua_kristianto144@outlook.com>
2025-08-13 10:46:15 +0700

    Added loader component

---
+++ src/components/Loader.vue
@@ -0,0 +1,6 @@
+<!-- src/components/Loader.vue -->
+<template>
+  <div class="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-10000">
+    <div class="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-fluorescent"></div>
+  </div>
+</template>
