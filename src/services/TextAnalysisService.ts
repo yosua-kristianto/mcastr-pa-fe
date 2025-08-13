71b040b32416af12206b893068facf2bb42c5888
Yosua Kristianto (Legion) <yosua_kristianto144@outlook.com>
2025-08-13 10:46:07 +0700

    Added Text Analysis Service integration

---
+++ src/services/TextAnalysisService.ts
@@ -0,0 +1,16 @@
+
+class TextAnalysisServices {
+
+    /**
+     * Data to be analyzed for text analysis.
+     * 
+     * @param data 
+     * @returns 
+     */
+    public textAnalysis(data: string): string {
+        return "https://picsum.photos/400/300";
+    }
+
+}
+
+export default new TextAnalysisServices();
