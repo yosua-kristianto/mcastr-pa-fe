5284f238b2591f8b937c9a45f13ff3c03b63ce27
Yosua Kristianto (Mac) <yosua_kristianto144@outlook.com>
2025-09-03 02:09:28 +0700

    Added env interface

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_API_URI: string;
+  readonly VITE_SECRET: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
